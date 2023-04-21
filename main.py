8283ab4765b11fbd7b7ba7aa41a1e3ca32a90044
Kusalava0 <dkb03122001@gmail.com>
2023-04-21 21:23:20 +0530

    solution

---
+++ main.py
@@ -0,0 +1,42 @@
+list_1 = [
+    {"id": "1", "name": "Shrey", "age": 25},
+    {"id": "3", "age": 10, "name": "Hello"},
+    {"id": "2", "name": "World", "age": 24},
+]
+
+list_2 = [
+    {"id": "1", "marks": 100},
+    {
+        "id": "3",
+        "marks": 90,
+        "roll_no": 11,
+        "extra_info": {
+            "hello": "world",
+        },
+    },
+]
+
+
+def merge_lists(list_1, list_2) -> list:
+    """
+    Complete this function, by merging the information from list_1 and list_2
+    to create a new list, which has all the information about each student from
+    both lists in one single dict.
+
+    - Both lists are unsorted
+    - Both lists can have missing values (for ex list_2 has missing id=2)
+    """
+    list_3 = []
+
+    for dict_1 in list_1:
+        for dict_2 in list_2:
+            if dict_1['id'] == dict_2['id']:
+                dict_1.update(dict_2)
+                break
+        list_3.append(dict_1)
+
+    return list_3
+
+
+list_3 = merge_lists(list_1, list_2)
+print(list_3)
